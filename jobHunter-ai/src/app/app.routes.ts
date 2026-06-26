217d487cb6f3a17824da38e7ec8cc17c68a04ff1
Alberto Córdoba <albertocordoba98@gmail.com>
2026-06-26 08:48:41 +0200

    feat(project-setup): create components and routing

--- jobHunter-ai/src/app/app.routes.ts
+++ jobHunter-ai/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'search',
+    loadComponent: () => import('./features/search/search').then((m) => m.Search),
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./features/profile/profile').then((m) => m.Profile),
+  },
+  {
+    path: 'assistant',
+    loadComponent: () => import('./features/assistant/assistant').then((m) => m.Assistant),
+  },
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./features/search/search').then((m) => m.Search),
+  },
+];
